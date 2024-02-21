ff4d69d2f247a60fdc1a4ae01493f6405b6f4e6e
Robert <Robert.pinney@ecit.no>
2024-02-21 09:19:54 +0100

    handle functions

--- src/App.js
+++ src/App.js
@@ -26,8 +26,6 @@ export default function App() {
     );
 }
 
-console.log(<DifferentContent test={23} />);
-
 function Tabbed({ content }) {
     const [activeTab, setActiveTab] = useState(0);
 
@@ -62,11 +60,25 @@ function Tab({ num, activeTab, onClick }) {
 }
 
 function TabContent({ item }) {
-    const [showDetails, setShowDetails] = useState(false);
+    const [showDetails, setShowDetails] = useState(true);
     const [likes, setLikes] = useState(0);
 
+
+    function handleTripleInc() {
+        setLikes((likes) => likes + 3);
+    }
+
     function handleInc() {
-        setLikes(likes + 1);
+        setLikes((likes) => likes + 1);
+    }
+
+    function handleUndo() {
+        setLikes(0);
+        setShowDetails(true);
+    }
+
+    function handleUndoLater() {
+        setTimeout(handleUndo, 2000);
     }
 
     return (
@@ -82,13 +94,13 @@ function TabContent({ item }) {
                 <div className="hearts-counter">
                     <span>{likes} ❤️</span>
                     <button onClick={handleInc}>+</button>
-                    <button>+++</button>
+                    <button onClick={handleTripleInc}>+++</button>
                 </div>
             </div>
 
             <div className="tab-undo">
-                <button>Undo</button>
-                <button>Undo in 2s</button>
+                <button onClick={handleUndo}>Undo</button>
+                <button onClick={handleUndoLater}>Undo in 2s</button>
             </div>
         </div>
     );
